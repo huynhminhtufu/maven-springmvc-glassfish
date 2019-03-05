1d763b32a90780441171716be55f9c896b8ab591
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-03-05 23:17:39 +0700

    various update

---
+++ out/artifacts/MavenSpringMVC_war_exploded/views/home.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Hello Home!</h2>
+</body>
+</html>
--- src/main/java/com/mrhmt/controller/HomeController.java
+++ src/main/java/com/mrhmt/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.mrhmt.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @RequestMapping("/")
     public String home() {
         return "Home";
     }
